05cd14e266ba16a4936bcc883d9b63160bb157cd
Torleif <torleif@geekality.net>
2012-09-13 09:57:28 +0200

    Updated assembly info and some comments.

--- MultiFileWatcher/Properties/AssemblyInfo.cs
+++ MultiFileWatcher/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("MultiFileWatcher")]
-[assembly: AssemblyDescription("Wrapper around FileSystemWatcher to watch multiple single files across directories")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyDescription("A wrapper around FileSystemWatcher to watch multiple single files across directories")]
+[assembly: AssemblyCompany("Torleif Berger")]
 [assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.*")]
