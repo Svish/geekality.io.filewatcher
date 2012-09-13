e0a268e59c955b5b857031912544f517c35d49ab
Torleif <torleif@geekality.net>
2012-09-13 13:09:49 +0200

    NuGet adjustments

--- MultiFileWatcher/Properties/AssemblyInfo.cs
+++ MultiFileWatcher/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Reflection;
 
-[assembly: AssemblyTitle("MultiFileWatcher")]
-[assembly: AssemblyDescription("A wrapper around FileSystemWatcher to watch multiple single files across directories")]
-[assembly: AssemblyCompany("Torleif Berger")]
+[assembly: AssemblyTitle("Multi File Watcher")]
+[assembly: AssemblyDescription("A simple wrapper around FileSystemWatcher to watch multiple single files across directories")]
 [assembly: CLSCompliant(true)]
 
 [assembly: AssemblyVersion("1.0.*")]
